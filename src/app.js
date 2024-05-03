7caf914ae3a4cadc953da3d699c2a50f28d0a9bd
GwenTogni <gwen.togni@student.supsi.ch>
2024-05-03 13:14:58 +0200

    Insert map and content

---
+++ src/app.js
@@ -0,0 +1,108 @@
+
+/* Text description */
+
+function city001(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'STABIO';
+  document.getElementById('generic').innerHTML = 'Prima città. Il punto di partenza per ogni allenatorə Pokémon!';
+  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Laboratorio Pokémon';
+}
+function city002(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'CHIASSO';
+  document.getElementById('generic').innerHTML = 'Seconda città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: veleno';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city003(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'MENDRISIO';
+  document.getElementById('generic').innerHTML = 'Terza città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: spettro.';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city004(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'CAMPIONE';
+  document.getElementById('generic').innerHTML = 'Quarta città. È all`interno della regione ma non fa propriamente parte della regione, quindi per accedervi devi dimostrare di aver sconfitto almeno due palestre.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Casinò Pokémon';
+}
+function city005(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'LUGANO';
+  document.getElementById('generic').innerHTML = 'Quinta città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: erba.';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Centro Commerciale';
+}
+function city006(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'RIVERA';
+  document.getElementById('generic').innerHTML = 'Sesta città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: acciaio';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city007(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'BELLINZONA';
+  document.getElementById('generic').innerHTML = 'Settima città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: volante';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Torre Pokémon';
+}
+function city008(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'TENERO';
+  document.getElementById('generic').innerHTML = 'Ottava città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Arena Pokéathlon';
+}
+function city009(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'SONOGNO';
+  document.getElementById('generic').innerHTML = 'Nona città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city010(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'MAGGIA';
+  document.getElementById('generic').innerHTML = 'Decima città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city011(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'LOCARNO';
+  document.getElementById('generic').innerHTML = 'Undicesima città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: coleottero.';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city012(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'BIASCA';
+  document.getElementById('generic').innerHTML = 'Dodicesima città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city013(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'AMBRÌ';
+  document.getElementById('generic').innerHTML = 'Tredicesima città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: ghiaccio';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city014(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'AIROLO';
+  document.getElementById('generic').innerHTML = 'Quattordicesima città.';
+  document.getElementById('gym').innerHTML = 'PALESTRA: roccia';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+function city015(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PASSO DEL GOTTARDO';
+  document.getElementById('generic').innerHTML = 'Il traguardo di tuttə lə allenatorə Pokémon!';
+  document.getElementById('gym').innerHTML = 'PALESTRA: Lega Pokémon';
+  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
