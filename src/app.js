d8c72e6b8ed6c60d8d4e1a086ad60d7c92f84768
GwenTogni <gwen.togni@student.supsi.ch>
2024-05-03 16:58:43 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ function city001(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'STABIO';
   document.getElementById('generic').innerHTML = 'Prima città. Il punto di partenza per ogni allenatorə Pokémon!';
-  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('gym').innerHTML = '';
   document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Laboratorio Pokémon';
 }
 function city002(){
@@ -17,20 +17,20 @@ function city002(){
   document.getElementById('title').innerHTML = 'CHIASSO';
   document.getElementById('generic').innerHTML = 'Seconda città.';
   document.getElementById('gym').innerHTML = 'PALESTRA: veleno';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('special').innerHTML = '';
 }
 function city003(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'MENDRISIO';
-  document.getElementById('generic').innerHTML = 'Terza città.';
+  document.getElementById('generic').innerHTML = 'Terza città. Città fantasma.';
   document.getElementById('gym').innerHTML = 'PALESTRA: spettro.';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('special').innerHTML = '';
 }
 function city004(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'CAMPIONE';
   document.getElementById('generic').innerHTML = 'Quarta città. È all\'interno della regione ma non fa propriamente parte della regione, quindi per accedervi devi dimostrare di aver sconfitto almeno due palestre.';
-  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('gym').innerHTML = '';
   document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Casinò Pokémon';
 }
 function city005(){
@@ -45,7 +45,7 @@ function city006(){
   document.getElementById('title').innerHTML = 'RIVERA';
   document.getElementById('generic').innerHTML = 'Sesta città.';
   document.getElementById('gym').innerHTML = 'PALESTRA: acciaio';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('special').innerHTML = '';
 }
 function city007(){
   document.getElementById('description').style.display = "block";
@@ -58,57 +58,57 @@ function city008(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'TENERO';
   document.getElementById('generic').innerHTML = 'Ottava città.';
-  document.getElementById('gym').innerHTML = 'PALESTRA: -';
+  document.getElementById('gym').innerHTML = '';
   document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Arena Pokéathlon';
 }
 function city009(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'SONOGNO';
   document.getElementById('generic').innerHTML = 'Nona città.';
-  document.getElementById('gym').innerHTML = 'PALESTRA: -';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
 }
 function city010(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'MAGGIA';
   document.getElementById('generic').innerHTML = 'Decima città.';
-  document.getElementById('gym').innerHTML = 'PALESTRA: -';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
 }
 function city011(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'LOCARNO';
   document.getElementById('generic').innerHTML = 'Undicesima città.';
   document.getElementById('gym').innerHTML = 'PALESTRA: coleottero.';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('special').innerHTML = '';
 }
 function city012(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'BIASCA';
-  document.getElementById('generic').innerHTML = 'Dodicesima città.';
-  document.getElementById('gym').innerHTML = 'PALESTRA: -';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('generic').innerHTML = 'Dodicesima città. Si narra che in passato venne distrutta da un pokémon leggendario di tipo acqua...';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
 }
 function city013(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'AMBRÌ';
   document.getElementById('generic').innerHTML = 'Tredicesima città.';
   document.getElementById('gym').innerHTML = 'PALESTRA: ghiaccio';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('special').innerHTML = '';
 }
 function city014(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'AIROLO';
   document.getElementById('generic').innerHTML = 'Quattordicesima città.';
   document.getElementById('gym').innerHTML = 'PALESTRA: roccia';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('special').innerHTML = '';
 }
 function city015(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'PASSO DEL GOTTARDO';
   document.getElementById('generic').innerHTML = 'Il traguardo di tuttə lə allenatorə Pokémon!';
   document.getElementById('gym').innerHTML = 'PALESTRA: Lega Pokémon';
-  document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+  document.getElementById('special').innerHTML = '';
 }
 
 /* Text description route */
@@ -214,7 +214,7 @@ function route014(){
 function route015(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'PERCORSO 015';
-  document.getElementById('generic').innerHTML = '';
+  document.getElementById('generic').innerHTML = 'In passato vi fu una grande battaglia tra pokémon leggendari di tipo roccia e lotta. Ancora oggi, per superare il passaggio, è necessario usare FORZA per spostare dei massi rimasti in terra a seguito dello scontro.';
   document.getElementById('gym').innerHTML = '';
   document.getElementById('special').innerHTML = '';
 }
@@ -270,7 +270,7 @@ function place003(){
 function place004(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'VIA VITTORIA';
-  document.getElementById('generic').innerHTML = '';
+  document.getElementById('generic').innerHTML = 'Il cammino potrebbe essere rallentato a causa di pokémon provenienti da altre regioni. Per questo motivo, solo ai migliori allenatori è consentito il transito.';
   document.getElementById('gym').innerHTML = '';
   document.getElementById('special').innerHTML = '';
 }
