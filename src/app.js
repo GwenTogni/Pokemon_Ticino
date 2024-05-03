5666c92a951a58e26c7a7109ab48ac11b6ee18d1
GwenTogni <gwen.togni@student.supsi.ch>
2024-05-03 14:59:55 +0200

    Add route and places

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 
-/* Text description */
+function leaveDescription(){
+  document.getElementById('description').style.display = "none"
+}
+
+/* Text description city */
 
 function city001(){
   document.getElementById('description').style.display = "block";
@@ -25,7 +29,7 @@ function city003(){
 function city004(){
   document.getElementById('description').style.display = "block";
   document.getElementById('title').innerHTML = 'CAMPIONE';
-  document.getElementById('generic').innerHTML = 'Quarta città. È all`interno della regione ma non fa propriamente parte della regione, quindi per accedervi devi dimostrare di aver sconfitto almeno due palestre.';
+  document.getElementById('generic').innerHTML = 'Quarta città. È all\'interno della regione ma non fa propriamente parte della regione, quindi per accedervi devi dimostrare di aver sconfitto almeno due palestre.';
   document.getElementById('gym').innerHTML = 'PALESTRA: -';
   document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: Casinò Pokémon';
 }
@@ -105,4 +109,168 @@ function city015(){
   document.getElementById('generic').innerHTML = 'Il traguardo di tuttə lə allenatorə Pokémon!';
   document.getElementById('gym').innerHTML = 'PALESTRA: Lega Pokémon';
   document.getElementById('special').innerHTML = 'EDIFICI SPECIALI: -';
+}
+
+/* Text description route */
+
+function route001(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 001';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route002(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 002';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route003(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 003';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route004(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 004';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route005(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 005';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route006(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 006';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route007(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 007';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route008(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 008';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route009(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 009';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route010(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 010';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route011(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 011';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route012(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 012';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route013(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 013';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route014(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 014';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route015(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 015';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route016(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 016';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route017(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 017';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+function route018(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'PERCORSO 018';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+
+
+/* Text description places */
+
+function place001(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'MONTE CENERI';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+
+function place002(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'ZONA SAFARI';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+
+function place003(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'CENTRALE ELETTRICA';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
+}
+
+function place004(){
+  document.getElementById('description').style.display = "block";
+  document.getElementById('title').innerHTML = 'VIA VITTORIA';
+  document.getElementById('generic').innerHTML = '';
+  document.getElementById('gym').innerHTML = '';
+  document.getElementById('special').innerHTML = '';
 }
